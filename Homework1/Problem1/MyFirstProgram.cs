1d3290667294ba401da44777a5769a2683216025
jazi2018 <jaredaziv@gmail.com>
2024-08-24 19:51:22 -0700

    Made minor adjustments to problem 1, wrote out problem 2 and included a screenshot of its success

--- Homework1/Problem1/MyFirstProgram.cs
+++ Homework1/Problem1/MyFirstProgram.cs
@@ -20,7 +20,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApplication1
+namespace Problem1
 {
     class MyFirstProgram
     {
---
+++ Homework1/Problem2/Program.cs
@@ -0,0 +1,29 @@
+﻿/// Homework 1		Exercise 2
+/// File Name: MySecondProgram.cs
+/// @author: Jared Ziv
+/// Date:  August 24, 2024
+///
+/// Problem Statement: Display a simple picture of your choice.
+/// 
+/// 
+/// Overall Plan:
+/// 1) Find an example on asciiart.eu
+/// 2) Paste each line into a WriteLine function
+/// 3) Include a ReadLine function at the end so the image is visible
+
+using System;
+
+namespace Problem2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" ______________________________________________________|_._._._._._._._._._.");
+            Console.WriteLine("\\______________________________________________________|_#_#_#_#_#_#_#_#_#_|");
+            Console.WriteLine("                                                       |");
+
+            Console.ReadLine();
+        }
+    }
+}
