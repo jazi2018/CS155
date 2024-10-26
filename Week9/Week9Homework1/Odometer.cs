def1cd562076052a1c04afd41e285ed8e5b31c24
jazi2018 <jaredaziv@gmail.com>
2024-10-26 00:57:06 -0700

    Finished week 9 homework 1

---
+++ Week9/Week9Homework1/Odometer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week9Homework1
+{
+    internal class Odometer
+    {
+        private double _miles;
+        private double _gallons;
+
+        //default constructor
+        public Odometer() { _miles = 0; _gallons = 0; }
+        //parameterized constructor
+        public Odometer(double miles, double gallons)
+        { _miles = miles; _gallons = gallons; }
+        
+        public double Miles { get { return _miles; }
+                              //prevent miles from being negative
+                              set { _miles = value < 0 ? 0 : value; } }
+        public double Gallons { get { return _gallons; }
+                                //prevent gallons from being negative
+                                set { _gallons = value < 0 ? 0 : value; } }
+
+        public void Reset() { _miles = 0; _gallons = 0; }
+        public void Add(double miles, double gallons)
+        { _miles += miles; _gallons += gallons; }
+        public double CalcMPG()
+        {
+            if (_gallons <= 0) return 0;
+            else return _miles / _gallons;
+        }
+        public string ToString()
+        {
+            return String.Format("Driven {0:0.00} miles and consumed {1:0.00} gallons of fuel.\n" +
+                                 "Total MPG: {2:0.00}", _miles, _gallons, CalcMPG());
+        }
+    }
+}
---
+++ Week9/Week9Homework1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Week9Homework1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Odometer car1 = new Odometer();
+            Console.WriteLine("New car initialized");
+            car1.Miles = 25.3;
+            car1.Gallons = 2.1;
+            Console.WriteLine("Car1 driven. New data: \n" +
+                car1.ToString());
+
+            Odometer car2 = new Odometer(79576.55, 3287.04);
+            Console.WriteLine("Old car:\n"+
+                car2.ToString());
+            Console.WriteLine("Adding 45 miles and 3 gallons to car2:");
+            car2.Add(45, 3);
+            Console.WriteLine(car2.ToString());
+            Console.WriteLine("Resetting car2:");
+            car2.Reset();
+            Console.WriteLine(car2.ToString());
+        }
+    }
+}
