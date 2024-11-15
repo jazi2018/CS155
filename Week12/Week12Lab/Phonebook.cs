4619ec027cb93915810be10e931ce10b2236386d
jazi2018 <jaredaziv@gmail.com>
2024-11-15 12:33:12 -0800

    Finished week 12 lab

---
+++ Week12/Week12Lab/Phonebook.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week12Lab
+{
+    internal class Phonebook
+    {
+        //key = first name, value = phone number (formatted (###) ### - ####)
+        private Dictionary<string, string> book;
+
+        public Phonebook()
+        {
+            //initialize book
+            book = new Dictionary<string, string>();
+        }
+
+        public void Lookup(string name)
+        {
+            if (book.ContainsKey(name))
+            {
+                Console.WriteLine("{0}: {1}", name, book[name]);
+            }
+            else
+            {
+                //name does not have a number in the phonebook
+                Console.WriteLine("No entry found for {0} in this phonebook.", name);
+            }
+        }
+
+        public void Add(string name, string number)
+        {
+            if (!book.ContainsKey(name))
+            {
+                book.Add(name, number);
+                Console.WriteLine("Added {0} with phone number {1}.", name, number);
+            }
+            else
+            {
+                Console.WriteLine("{0} already has an entry in this phonebook.", name);
+            }
+        }
+
+        public void Remove(string name)
+        {
+            if (book.ContainsKey(name))
+            {
+                book.Remove(name);
+                Console.WriteLine("{0} has been removed from this phonebook.", name);
+            }
+            else
+            {
+                Console.WriteLine("No entry found for {0} in this phonebook.", name);
+            }
+        }
+
+        public void ListAll()
+        {
+            Console.WriteLine("Displaying {0} entries:", book.Count);
+            foreach (KeyValuePair<string, string> obj in book)
+            {
+                Console.WriteLine("{0}: {1}", obj.Key, obj.Value);
+            }
+        }
+
+        public void Clear()
+        {
+            Console.WriteLine("Removing {0} entries from this phonebook.", book.Count);
+            book.Clear();
+        }
+    }
+}
---
+++ Week12/Week12Lab/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace Week12Lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Phonebook phonebook = new Phonebook();
+
+            while (true)
+            {
+                Console.WriteLine("\nPhone Lookup Program");
+                Console.WriteLine("1. Add Contact");
+                Console.WriteLine("2. Delete Contact");
+                Console.WriteLine("3. Find Phone Number");
+                Console.WriteLine("4. Display All Contacts");
+                Console.WriteLine("5. Exit");
+                Console.Write("Enter your choice (1-5): ");
+
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        Console.Write("Enter name: ");
+                        string name = Console.ReadLine();
+                        Console.Write("Enter phone number: ");
+                        string phoneNumber = Console.ReadLine();
+                        phonebook.Add(name, phoneNumber);
+                        break;
+
+                    case "2":
+                        Console.Write("Enter name to delete: ");
+                        name = Console.ReadLine();
+                        phonebook.Remove(name);
+                        break;
+
+                    case "3":
+                        Console.Write("Enter name to find: ");
+                        name = Console.ReadLine();
+                        phonebook.Lookup(name);
+                        break;
+
+                    case "4":
+                        phonebook.ListAll();
+                        break;
+
+                    case "5":
+                        Console.WriteLine("Exiting program. Goodbye!");
+                        return;
+
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
+                }
+            }
+        }
+    }
+}
