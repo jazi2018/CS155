20702f5a9b32d7dc278bb7eb2dcbdffde2c836d9
jazi2018 <jaredaziv@gmail.com>
2024-12-02 18:16:27 -0800

    Completed week 15 homework

---
+++ Week15/Week15_Homework/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Week15_Homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
